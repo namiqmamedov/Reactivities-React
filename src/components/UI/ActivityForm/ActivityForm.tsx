ddf7321a726b5f3c38d29dadd26ce242875263e2
namiqmamedov <namigmm@code.edu.az>
2023-07-28 01:47:25 +0400

    Add formik

--- src/components/UI/ActivityForm/ActivityForm.tsx
+++ src/components/UI/ActivityForm/ActivityForm.tsx
@@ -8,19 +8,20 @@ import { Activity } from "../../../models/activity";
 import Loading from "../../../common/Loading";
 import {v4 as uuid} from 'uuid'
 import { Link } from "react-router-dom";
+import { Field, Formik } from "formik";
 
 
 
 export default observer(function ActivityForm() {
 
   const {activityStore} = useStore();
-  const {createActivity, updateActivity, 
+  const {
     loading, loadActivity,loadingInitial} = activityStore;
 
     const {id} = useParams();
-    const navigate = useNavigate()
+    // const navigate = useNavigate()
 
-    const [activity,setActivity] = useState<Activity>({
+    const [activity,setActivity] = useState({
         id: '',
         title: '',
         category: '',
@@ -34,41 +35,44 @@ export default observer(function ActivityForm() {
       if(id) loadActivity(id).then(activity => setActivity(activity!))
     }, [id, loadActivity]);
 
-  function handleSubmit(e:React.FormEvent<EventTarget>) {
-    e.preventDefault()
-    if(!activity.id) {
-      activity.id = uuid();
-      createActivity(activity).then(() => navigate(`/activities/${activity.id}`))
-    }
-    else{
-      updateActivity(activity).then(() => navigate(`/activities/${activity.id}`))
-    }
-  }
+  // function handleSubmit(e:React.FormEvent<EventTarget>) {
+  //   e.preventDefault()
+  //   if(!activity.id) {
+  //     activity.id = uuid();
+  //     createActivity(activity).then(() => navigate(`/activities/${activity.id}`))
+  //   }
+  //   else{
+  //     updateActivity(activity).then(() => navigate(`/activities/${activity.id}`))
+  //   }
+  // }
 
-  function handleInputChange(event: ChangeEvent<HTMLInputElement | HTMLTextAreaElement>) {
-      const {name,value} = event.target;
-      setActivity({...activity, [name]: value})
-  }
+  // function handleInputChange(event: ChangeEvent<HTMLInputElement | HTMLTextAreaElement>) {
+  //     const {name,value} = event.target; 
+  //     setActivity({...activity, [name]: value})
+  // }
 
   if(loadingInitial) return <Loading/>
   
   return (
     <Container>
-      <form onSubmit={handleSubmit} autoComplete="off" className="bg-white py-4 px-4 mt-10">
-              <TextField id="outlined-basic" label="Title" variant="outlined" fullWidth={true} value={activity.title} name='title' onChange={handleInputChange} className="!mb-5" />
-              <TextField id="outlined-basic" label="Description" rows={2} multiline variant="outlined" fullWidth={true} value={activity.description} name='description' onChange={handleInputChange} className="!mb-5" />
-              <TextField id="outlined-basic" label="Category" variant="outlined" fullWidth={true} value={activity.category} name='category' onChange={handleInputChange} className="!mb-5" />
-              <TextField id="outlined-basic" label="Date" variant="outlined" fullWidth={true} value={activity.date} type="date" name='date' onChange={handleInputChange} className="!mb-5" />
-              <TextField id="outlined-basic" label="City" variant="outlined" fullWidth={true} value={activity.city} name='city' onChange={handleInputChange}className="!mb-5" />
-              <TextField id="outlined-basic" label="Venue" variant="outlined" fullWidth={true} value={activity.venue} name='venue' onChange={handleInputChange} className="!mb-5" />
+      <Formik initialValues={activity} onSubmit={values => console.log(values)} enableReinitialize >
+        {({handleSubmit}) => (
+       <form onSubmit={handleSubmit} autoComplete="off" className="bg-white py-4 px-4 mt-10 ui form">
+              <Field label="Title" variant="outlined"  name='title'  className="!mb-5" />
+              <Field label="Description" rows={2} multiline variant='outlined' name='description' className="!mb-5" />
+              <Field label="Category" variant="outlined" name='category'  className="!mb-5" />
+              <Field label="Date" variant="outlined" valueype="date" name='date'  className="!mb-5" />
+              <Field label="City" variant="outlined" valueame='city' className="!mb-5" />
+              <Field label="Venue" variant="outlined"  name='venue'  className="!mb-5" />
               <div className="form-button flex justify-end gap-3">
-                <Button component={Link} to='/activities'  variant="outlined" className="!bg-gray-500 !text-white !border-none" size="small">Cancel</Button>
-                {/* <Button type="submit" variant="contained" className="!bg-green-600" size="small">Submit</Button> */}
+              <Button component={Link} to='/activities'  variant="outlined" className="!bg-gray-500 !text-white !border-none" size="small">Cancel</Button>
               <LoadingButton loading={loading} type="submit" variant="contained" className="!bg-green-600" size="small">
               Submit
               </LoadingButton>
               </div>
-          </form>
+      </form>
+        )}
+      </Formik>
     </Container>
   )
 })
